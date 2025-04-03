c40ceeb88c9db1eae587f92627fa8ec5f455df19
Voidane <hero4aquest@gmail.com>
2025-04-03 13:31:53 -0400

    TrashItems are destroyed

---
+++ NoMoreTrash/NoMoreTrashMod.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+using Harmony;
+using Il2CppScheduleOne.Interaction;
+using Il2CppScheduleOne.ItemFramework;
+using Il2CppScheduleOne.PlayerScripts;
+using Il2CppScheduleOne.Trash;
+using MelonLoader;
+using UnityEngine;
+
+namespace NoMoreTrash
+{
+    public class NoMoreTrashMod : MelonMod
+    {
+        public static readonly string MOD_PREFIX = "[NoMoreTrash]";
+
+        public override void OnInitializeMelon()
+        {
+            MelonLogger.Msg($"{MOD_PREFIX} Initializing, Created by Voidane.");
+            MelonLogger.Msg($"{MOD_PREFIX} Discord: discord.gg/XB7ruKtJje");
+            HarmonyPatches();
+            MelonLogger.Msg($"{MOD_PREFIX} Has been initialized...");
+        }
+
+        private void HarmonyPatches()
+        {
+            HarmonyLib.Harmony patcher = new HarmonyLib.Harmony("com.voidane.nomoretrash");
+
+            patcher.Patch(typeof(TrashItem).GetMethod("Start"), null, new HarmonyLib.HarmonyMethod(
+                typeof(NoMoreTrashMod).GetMethod(nameof(Patch_TrashItem_Start), BindingFlags.Static | BindingFlags.NonPublic)),
+                null, null, null);
+        }
+
+        private static void Patch_TrashItem_Start(TrashItem __instance)
+        {
+            __instance.DestroyTrash();
+        }
+    }
+}
---
+++ NoMoreTrash/Properties/AssemblyInfo.cs
@@ -0,0 +1,41 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+using MelonLoader;
+using NoMoreTrash;
+
+[assembly: MelonInfo(typeof(NoMoreTrashMod), "NoMoreTrash", "1.0", "Voidane")]
+[assembly: MelonGame("TVGS", "Schedule I")]
+
+// General Information about an assembly is controlled through the following
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("NoMoreTrash")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("NoMoreTrash")]
+[assembly: AssemblyCopyright("Copyright ©  2025")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible
+// to COM components.  If you need to access a type in this assembly from
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("1457ace6-458c-4826-92c9-1aba0a169840")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Build and Revision Numbers
+// by using the '*' as shown below:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
